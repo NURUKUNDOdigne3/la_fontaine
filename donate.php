8a39edb5497ba3b3471be096d83eb747b2a9c238
mbarushimanafabrice <123445030+MbarushimanaFabrice@users.noreply.github.com>
2025-09-17 11:00:38 +0300

    fix donate page

--- donate.php
+++ donate.php
@@ -313,71 +313,76 @@ include "connection.php"
 </head>
 
 <body>        
-    <!-- Top Bar -->
-    <div class="top-bar d-none d-md-block">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="top-bar-left">
-                        <div class="text">
-                            <i class="fa fa-phone-alt"></i>
-                            <p>+250-787-691-062</p>
+   <body>        
+        <div class="top-bar d-none d-md-block">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="top-bar-left">
+                            <div class="text">
+                                <i class="fa fa-phone-alt"></i>
+                                <p>+250-787-691-062</p>
+                            </div>
+                            <div class="text">
+                                <i class="fa fa-envelope"></i>
+                                <p>info@lafontaine.org</p>
+                            </div>
                         </div>
-                        <div class="text">
-                            <i class="fa fa-envelope"></i>
-                            <p>info@lafontaine.org</p>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="top-bar-right">
+                            <div class="social">
+                                <a href=""><i class="fab fa-twitter"></i></a>
+                                <a href=""><i class="fab fa-linkedin-in"></i></a>
+                                <a href=""><i class="fab fa-instagram"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="top-bar-right">
-                        <div class="social">
-                            <a href=""><i class="fab fa-twitter"></i></a>
-                            <a href=""><i class="fab fa-linkedin-in"></i></a>
-                            <a href=""><i class="fab fa-instagram"></i></a>
-                        </div>
+            </div>
+        </div>
+        
+
+        <div  class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid">
+                <a href="index.html" class="navbar-brand"><img src="img/logo.png" alt=""></a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto">
+                        <a href="index.html" class="nav-item nav-link">Home</a>
+                        <a href="about.html" class="nav-item nav-link">About</a>
+                        <a href="donate.html" class="nav-item nav-link  active">Donate</a> 
+                        <a href="event.html" class="nav-item nav-link">Events</a>
+                        <a href="team.html" class="nav-item nav-link">Team</a>
+                       
+                        <a href="join.html" class="nav-item nav-link">Join us</a>
+
+                        <a href="contact.html"   class="nav-item nav-link">Contact</a>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
-        <div class="container-fluid">
-            <a href="index.php" class="navbar-brand"><img src="img/logo.png" alt="La Fontaine Community Center"></a>
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                <div class="navbar-nav ml-auto">
-                    <a href="index.php" class="nav-item nav-link">Home</a>
-                    <a href="about.php" class="nav-item nav-link">About</a>
-                    <a href="donate.php" class="nav-item nav-link active">Donate</a> 
-                    <a href="event.php" class="nav-item nav-link">Events</a>
-                    <a href="team.php" class="nav-item nav-link">Team</a>
-                    <a href="join.php" class="nav-item nav-link">Join us</a>
-                    <a href="contact.php" class="nav-item nav-link">Contact</a>
+        
+        
+        <!-- Page Header Start -->
+        <div class="page-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Donate Now</h2>
+                    </div>
+                    <div class="col-12">
+                        <a href="">Home</a>
+                        <a href="">Donate</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </nav>
-    
-    <!-- Page Header -->
-    <div class="page-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h2>Donate Now</h2>
-                </div>
-                <div class="col-12">
-                    <a href="index.php">Home</a>
-                    <a href="donate.php">Donate</a>
-                </div>
-            </div>
-        </div>
-    </div>
+        <!-- Page Header End -->
     
     <!-- Donation Section -->
     <div class="container">
